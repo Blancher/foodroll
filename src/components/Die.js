3f4b9ff8509cd34abcdce92bc6aecdbe3c087ea8
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-04-15 10:52:17 -0700

    Update Die.js

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,5 +1,5 @@
 import images from '../images/images';
 
 export default function Die(props) {
-    return <div className='dies' style={{backgroundImage: `url(${images[props.val - 1]})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover', border: props.bool && '3px solid #59E391'}} onClick={props.onClick}></div>;
+    return <div className='dies' style={{backgroundImage: `url(${images[props.val]})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover', border: props.bool && '3px solid #59E391'}} onClick={props.onClick}></div>;
 }
